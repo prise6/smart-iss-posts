04713d43d1131d3108fef85fc97b9c04b4ea83a1
Francois Vieille <vieille.francois@gmail.com>
2019-03-14 10:46:59 +0100

    variational model

--- iss/models/__init__.py
+++ iss/models/__init__.py
@@ -3,3 +3,4 @@ from .AbstractModel import AbstractModel
 from .AbstractModel import AbstractAutoEncoderModel
 from .SimpleConvAutoEncoder import SimpleConvAutoEncoder
 from .SimpleAutoEncoder import SimpleAutoEncoder
+from .VariationalAutoEncoder import VarAutoEncoder
